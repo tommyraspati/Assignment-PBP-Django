de635c5f41dcd0aaea5af02b48e60e07a10125f1
raspatimahatma <tommymahatma08@gmail.com>
2022-09-14 15:57:03 +0700

    Last commit(I hope)

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_catalog_item,
+        'nama': 'Raspati Mahatma K.D (Tommy)',
+        'id': '2106750244'
+    }
+    return render(request, "katalog.html", context)
